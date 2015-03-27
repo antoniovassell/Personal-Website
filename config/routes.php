c00051cf076d491d4c95a1dc3fd4490b47c776cc
Antonio Vassell <antoniovassell@gmail.com>
2015-03-27 04:45:06 +0000

    Add resume pages

--- config/routes.php
+++ config/routes.php
@@ -49,7 +49,8 @@ Router::scope('/', function ($routes) {
      */
     $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
     $routes->connect('/contact', ['controller' => 'Pages', 'action' => 'display', 'contact']);
-    $routes->connect('/portfolio', ['controller' => 'Pages', 'action' => 'display', 'contact']);
+    $routes->connect('/portfolio', ['controller' => 'Pages', 'action' => 'display', 'portfolio']);
+    $routes->connect('/resume', ['controller' => 'Pages', 'action' => 'display', 'resume']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
